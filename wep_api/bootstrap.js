939ab15706e2743b1730f361584f6b8d8bc63299
Roman Dibikhin <dibikhin@gmail.com>
2018-03-23 19:10:58 +0300

    closes #21. promises added, jshint rmvd, eslint added #22

--- wep_api/bootstrap.js
+++ wep_api/bootstrap.js
@@ -10,66 +10,57 @@ const ajv = new Ajv({ allErrors: true });
 
 const benalu = require('./node_modules/benalu/benalu'); // self built, due to old npm
 const mongodb = require('mongodb');
-const Cache = require('ttl');
+// const Cache = require('ttl');
 
 const treasurer_params_schemas = require('schemas/treasurer_params.json');
 
 const threshold_strategy = require('business_rules/threshold_strategy');
 
-const caching_strategy_factory = require('infrastructure/aspects_factories/caching_strategy_factory');
-const callback_validator_factory = require('infrastructure/aspects_factories/callback_validator_factory');
+// const caching_strategy_factory = require('infrastructure/aspects_factories/caching_strategy_factory');
 const params_validator_factory = require('infrastructure/aspects_factories/params_validator_factory');
-const error_handling_strategy_factory = require('infrastructure/aspects_factories/error_handling_strategy_factory');
+// const error_handling_strategy_factory = require('infrastructure/aspects_factories/error_handling_strategy_factory');
 
-const is_callback_valid = require('infrastructure/advice/callback_validator');
 const is_params_valid = require('infrastructure/advice/params_validator');
-const error_handling_strategy = require('infrastructure/advice/error_handling_strategy');
+// const error_handling_strategy = require('infrastructure/advice/error_handling_strategy');
 
 const ajv_helpers = require('infrastructure/ajv/helpers');
 const ajv_add_custom_keywords = require('infrastructure/ajv/custom_keywords');
-const helpers = require('infrastructure/helpers');
+// const helpers = require('infrastructure/helpers');
 
 const accounts_db_adapter_raw = require('db/accounts_db_adapter');
 const accounts_db_bootstrap = require('db/accounts_db_bootstrap');
-const cache_adapter = require('db/cache_adapter');
+// const cache_adapter = require('db/cache_adapter');
 
 const treasurer_raw = require('models/treasurer');
 
 console.info('Starting...');
 
-const cache = new Cache({ ttl: 30 * 1000 }); // TODO config
+// const cache = new Cache({ ttl: 30 * 1000 }); // TODO config
 
-// order matters, but how? caching_strategy_factory may break next interceptors
+// // order matters, but how? caching_strategy_factory may break next interceptors
 const accounts_db_adapter = benalu
     .fromInstance(accounts_db_adapter_raw)
-    .addInterception(
-        callback_validator_factory(
-            { helpers },
-            { callback_validator_advice: is_callback_valid }))
-    // TODO config cache 'enabled' option    
-    .addInterception(
-        caching_strategy_factory({ helpers, cache, cache_adapter }))
-    .addInterception(
-        error_handling_strategy_factory({ helpers, logger: console }, { error_handling_strategy }))
+    //     // // TODO config cache 'enabled' option    
+    //     // .addInterception(
+    //     //     caching_strategy_factory({ helpers, cache, cache_adapter }))
+    //     // .addInterception(
+    //     //     error_handling_strategy_factory({ helpers, logger: console }, { error_handling_strategy }))
+    //     // TODO params validation
     .build();
 
 ajv_add_custom_keywords({ ajv_helpers, ajv, mongodb });
 // add_keyword(ajv, { keyword: 'is_frozen_deep', is_valid: Object.isFrozenDeep });
 const treasurer_params_validators = ajv_helpers.compile_validators({ ajv, schemas: treasurer_params_schemas });
 
-// TODO 'treasurer' -> 'treasurer'
-
 const treasurer = benalu
     .fromInstance(treasurer_raw)
-    .addInterception(
-        callback_validator_factory({ helpers }, { callback_validator_advice: is_callback_valid }))
     .addInterception(
         params_validator_factory({
             params_validators: treasurer_params_validators,
             params_validator_advice: is_params_valid
         }))
-    .addInterception(
-        error_handling_strategy_factory({ helpers, logger: console }, { error_handling_strategy }))
+    //     // .addInterception(
+    //     //     error_handling_strategy_factory({ helpers, logger: console }, { error_handling_strategy }))
     .build();
 
 // addInterception validate business rules ?
@@ -88,15 +79,16 @@ const conn_opts = {
     collection_name: 'accounts'
 };
 
-accounts_db_bootstrap
-    .init(treasurer_ctx, conn_opts)
-    .then(run_test)
-    .catch(err => {
-        console.error('error', err);
-        process.exit(0);
-    });
+(async function () {
+    try {
+        const accounts_col = await accounts_db_bootstrap.init(treasurer_ctx, conn_opts);
+        await run_test(accounts_col);
+    } catch (err) {
+        console.log(err);
+    }
+})();
 
-function run_test(accounts_col) {
+async function run_test(accounts_col) {
     treasurer_ctx.db = { accounts: accounts_col };
 
     const op_id = uuidv1();
@@ -106,52 +98,46 @@ function run_test(accounts_col) {
         op_id
     });
 
-    treasurer.balance(treasurer_ctx, balance_params, (err, data) => {
-        if (!data) {
-            console.error('Nothing found');
-            console.error(balance_params);
-            process.exit(0);
-        }
-        console.info('b1=' + data.value.balance.toString());
+    let data = await treasurer.balance(treasurer_ctx, balance_params);
+    if (!data) {
+        console.error('Nothing found');
+        console.error(balance_params);
+        process.exit(0);
+    }
+    console.info('b1=' + data.value.balance.toString());
 
-        const withdraw_params = Object.freeze({ account_id: balance_params.account_id, outgoing: '0.125', op_id });
-        treasurer.withdraw(treasurer_ctx, withdraw_params, (err, data) => {
-            if (err) { console.error(err); process.exit(0); }
-            console.info(data.value.balance.toString());
+    const withdraw_params = Object.freeze({ account_id: balance_params.account_id, outgoing: '0.125', op_id });
+    data = await treasurer.withdraw(treasurer_ctx, withdraw_params);
 
-            setTimeout(() => {
-                treasurer.balance(treasurer_ctx, balance_params, (err, data) => {
-                    if (err) { console.error(err); }
-                    console.info('b2=' + data.value.balance.toString());
+    console.info(data.value.balance.toString());
 
-                    const deposit_params = Object.freeze({ account_id: balance_params.account_id, incoming: '1.0', op_id });
-                    treasurer.deposit(treasurer_ctx, deposit_params, (err, data) => {
-                        if (err) { console.error(err); }
-                        console.info(data.value.balance.toString());
+    setTimeout(async () => {
+        data = await treasurer.balance(treasurer_ctx, balance_params);
 
-                        treasurer.balance(treasurer_ctx, balance_params, (err, data) => {
-                            if (err) { console.error(err); }
-                            console.info('b3=' + data.value.balance.toString());
+        console.info('b2=' + data.value.balance.toString());
 
-                            const transfer_params = Object.freeze({
-                                from: '5a99b022b0a023125aaaae28',
-                                to: '5a9a954f24ca261c2e2fc032',
-                                tranche: '0.5',
-                                op_id
-                            });
-                            treasurer.transfer(treasurer_ctx, transfer_params, (err, data) => {
-                                console.info(data.from.value.balance.toString());
-                                console.info(data.to.value.balance.toString());
+        const deposit_params = Object.freeze({ account_id: balance_params.account_id, incoming: '1.0', op_id });
+        data = await treasurer.deposit(treasurer_ctx, deposit_params);
 
-                                console.info('Finished.');
-                                process.exit(0);
-                            });
-                        });
-                    });
-                });
-            }, 1000);
+        console.info(data.value.balance.toString());
+
+        data = await treasurer.balance(treasurer_ctx, balance_params);
+
+        console.info('b3=' + data.value.balance.toString());
+
+        const transfer_params = Object.freeze({
+            from: '5a99b022b0a023125aaaae28',
+            to: '5a9a954f24ca261c2e2fc032',
+            tranche: '0.5',
+            op_id
         });
-    });
+        data = await treasurer.transfer(treasurer_ctx, transfer_params);
+        console.info(data.from.value.balance.toString());
+        console.info(data.to.value.balance.toString());
+
+        console.info('Finished.');
+        process.exit(0);
+    }, 1000);
 }
 
 // .then(() => {
--- wep_api/lib/infrastructure/aspects_factories/params_validator_factory.js
+++ wep_api/lib/infrastructure/aspects_factories/params_validator_factory.js
@@ -5,6 +5,6 @@ function create(params) {
         const invocation_params = invocation.parameters[1];
         const validate = params.params_validators[invocation.memberName];
         params.params_validator_advice({ target: invocation_params, validate, member_name: invocation.memberName });
-        invocation.proceed();
+        return invocation.proceed();
     };
 }
