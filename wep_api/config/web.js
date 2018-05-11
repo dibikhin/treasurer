9610466803b98a3d66ba20219f1f80f8f412c655
Roman Dibikhin <dibikhin@gmail.com>
2018-05-11 20:20:55 +0300

    Demo polished. Works locally and at now.sh. Closes #47

--- wep_api/config/web.js
+++ wep_api/config/web.js
@@ -1,38 +1,39 @@
-const web = {};
-web.port = process.env.PORT || 8080;
+module.exports = _init();
 
-web.swagger = {
-    ui_path: 'web/swagger.yaml',
-    controllers_path: 'components/treasurer/controllers',
-    use_stubs: false,
-    host: _compose_host,
-    validator: { validateResponse: true }
-};
+function _init() {
+    const web = {};
+    web.port = process.env.PORT || 8080;
 
-web.to_swagger_opts = _to_swagger_opts;
-web.swagger.configure_doc = _configure_doc;
+    web.swagger = {
+        ui_path: 'web/treasurer_api.yaml',
+        controllers_path: 'components/treasurer/controllers',
+        use_stubs: false,
+        compose_host: compose_host,
+        validator: { validateResponse: true }
+    };
 
-function _compose_host(default_host) {
-    // TODO localhost vs demohost
-    return process.env.NODE_ENV === 'development' ? 'treasurer-demo-1.now.sh' : default_host;
+    web.to_swagger_opts = to_swagger_opts;
+    web.swagger.configure_doc = configure_doc;
+    return web;
 }
 
-function _configure_doc({ fs, js_yaml }) {
-    const spec = fs.readFileSync(web.swagger.ui_path, 'utf8');
+function compose_host(default_host) {
+    return process.env.NOW_URL ? process.env.NOW_URL.replace('https://', '') : default_host;
+}
+
+function configure_doc({ fs, js_yaml, port, swagger }) {
+    const spec = fs.readFileSync(swagger.ui_path, 'utf8');
     const swagger_doc = js_yaml.safeLoad(spec);
-    // TODO fix closure magic
-    swagger_doc.host = web.swagger.host(swagger_doc.host);
+    swagger_doc.host = swagger.compose_host('localhost:' + port); // swagger_doc.host
     return swagger_doc;
 }
 
-function _to_swagger_opts(params) {
+function to_swagger_opts({ port, swagger }, controllers) {
     return {
-        port: params.port,
-        swaggerUi: params.swagger.ui_path,
-        controllers: params.swagger.controllers_path,
-        useStubs: params.swagger.use_stubs,
-        validator: params.swagger.validator
+        port: port,
+        swaggerUi: swagger.ui_path,
+        controllers,
+        useStubs: swagger.use_stubs,
+        validator: swagger.validator
     };
 }
-
-module.exports = web;
--- wep_api/web/app.js
+++ wep_api/web/app.js
@@ -2,8 +2,9 @@ module.exports = {
     config
 };
 
-async function config({ app, app_ctx, serve_static, no_cache, cors, json, uuidv1 }) {
+async function config({ app, app_ctx, favicon, serve_static, no_cache, cors, json, generate_op_id }) {
     app.use(no_cache()); // should be first
+    app.use(favicon('public/favicon.ico'));
     app.use(serve_static('public'));
     app.use(cors());
     app.use(json());
@@ -11,14 +12,19 @@ async function config({ app, app_ctx, serve_static, no_cache, cors, json, uuidv1
         req.ctx = app_ctx;
         next();
     });
-
-    // TODO add op_id strategy
     app.use(function set_op_id(req, res, next) {
-        req.op_id = uuidv1();
+        req.op_id = _get_op_id({ req, generate_op_id });
         next();
     });
     app.use(function set_headers(req, res, next) {
         res.setHeader('X-Request-ID', req.op_id);
         next();
     });
+}
+
+/**
+ * @private
+ */
+function _get_op_id({ req, generate_op_id }) {
+    return req.headers['x-now-id'] || req.headers['x-request-id'] || generate_op_id();
 }
